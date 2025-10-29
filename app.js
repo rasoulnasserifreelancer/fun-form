fc00383327623fa7a723c5d826df2796734e89a4
rasoulnasserifreelancer <rasoulnasseri_freelancer@yahoo.com>
2025-10-29 18:16:23 +0330

    i added the logic and style and it works fine,

---
+++ app.js
@@ -0,0 +1,85 @@
+window.addEventListener("load", handleFaceElementBorder);
+window.addEventListener("resize", handleFaceElementBorder);
+const formElement = document.querySelector("form");
+
+document.addEventListener("mouseenter", handleFollowMouse);
+document.addEventListener("mouseleave", closeEye)
+formElement.addEventListener("mouseenter", handleFormEnter);
+formElement.addEventListener("mouseleave", handleFormLeave);
+
+const formWidth = (window.innerWidth * 68) / 100;
+const formHeight = (window.innerHeight * 72) / 100;
+
+function handleFollowMouse() {
+  openEye()
+  const corneas = document.querySelectorAll(".cornea");
+  const eye = document.querySelector(".eye");
+  corneas.forEach((cornea) => {
+    window.addEventListener("mousemove", (event) => {
+      const x = event.clientX;
+      const y = event.clientY;
+      const rec = formElement.getBoundingClientRect();
+
+      if (x - rec.x > formWidth) {
+        cornea.style.left = "78%";
+      } else if (x < rec.x) {
+        cornea.style.left = 0;
+      } else {
+        cornea.style.left =
+          (x - rec.x) / formWidth < 22 / 100
+            ? 0
+            : ((x - rec.x) / formWidth) * 100 - 15 + "%";
+      }
+
+      if (y - rec.y > formHeight) {
+        cornea.style.bottom = "-10%";
+      } else if (y < rec.y) {
+        cornea.style.top = "10%";
+      } else {
+        cornea.style.top =
+          (y - rec.y) / formHeight < 22 / 100
+            ? 0
+            : ((y - rec.y) / formHeight) * 100 - 15 + "%";
+      }
+    });
+  });
+}
+
+function handleFormLeave() {
+  openEye();
+}
+
+function handleFormEnter() {
+  closeEye();
+}
+
+function closeEye() {
+  const eyeSpanElements = document.querySelectorAll(".eyelid");
+  eyeSpanElements.forEach((eye) => {
+    eye.classList.add("close");
+    eye.classList.remove("open");
+  });
+  const corneas = document.querySelectorAll(".cornea");
+  corneas.forEach((cornea) => {
+    cornea.style.display = "none";
+  });
+}
+
+function openEye() {
+  const eyeSpanElements = document.querySelectorAll(".eyelid");
+  eyeSpanElements.forEach((eye) => {
+    eye.classList.add("open");
+    eye.classList.remove("close");
+  });
+    const corneas = document.querySelectorAll(".cornea");
+  corneas.forEach((cornea) => {
+    cornea.style.display = "inline";
+  });
+}
+
+function handleFaceElementBorder() {
+  const faceElement = document.getElementById("face");
+  faceElement.style.width = (window.innerHeight * 3.5) / 10 + "px";
+  faceElement.style.height = (window.innerHeight * 3.4) / 10 + "px";
+  faceElement.style.borderRadius = (window.innerHeight * 2) / 10 + "px";
+}
